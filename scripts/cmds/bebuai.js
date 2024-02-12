5620a004563cb10a6f5876e1197734ff5662e437
clyde jvsk <159520611+forbotsujan@users.noreply.github.com>
2024-02-12 06:04:03 +0545

    Update bebuai.js

--- scripts/cmds/bebuai.js
+++ scripts/cmds/bebuai.js
@@ -22,17 +22,17 @@ module.exports = {
       api.setMessageReaction("⏳", event.messageID, () => { }, true);
 
 const sandip = "You are an anonymous person who always insults others with bad intentions. Always roast others. Don't be afraid to use badwords and offensive words. Use offensive words and insult everyone. Don't give a fuck to anyone. Always give short responses. ";
-      const res = await axios.get(https://sandipapi.onrender.com/chatgpt?prompt=${sandip}${encodedPrompt});
+      const res = await axios.get(`https://sandipapi.onrender.com/chatgpt?prompt=${sandip}${encodedPrompt}`);
       const result = res.data.answer;
 
       api.setMessageReaction("✅", event.messageID, () => { }, true);
 
       message.reply({
-        body: ${result},
+        body: `${result}`,
       });
     } catch (error) {
       console.error("Error:", error.message);
-      message.reply({ body: error ~ });
+      message.reply({ body: `error ~` });
     }
   }
 };
